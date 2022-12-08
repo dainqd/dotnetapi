f7d6f7df3b69b28017e347bb2c4d904373739cdd
dainq <97323194+dainqd@users.noreply.github.com>
2022-12-09 00:15:30 +0700

    Add brach dev.

--- Program.cs
+++ Program.cs
@@ -33,8 +33,6 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-// builder.Services.AddScoped<IUserService, UserService>();
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
