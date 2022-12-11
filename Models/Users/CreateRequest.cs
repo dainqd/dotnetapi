61933a11c7722ab5aea2a8d65fc10748067c3af3
dainq <97323194+dainqd@users.noreply.github.com>
2022-12-12 01:32:45 +0700

    Day moi la commit fix login ok nhe

--- Models/Users/CreateRequest.cs
+++ Models/Users/CreateRequest.cs
@@ -5,16 +5,16 @@ namespace apidemo.Models.Users;
 
 public class CreateRequest
 {
-    public string FirstName { get; set; }
-    public string LastNaqme { get; set; }
+    public string FirstName { get; set; } = "";
+    public string LastNaqme { get; set; } = "";
     [Required]
     public string Username { get; set; }
-    public string Email { get; set; }
-    public string PhoneNumber { get; set; }
-    
+    public string Email { get; set; } = "";
+    public string PhoneNumber { get; set; } = "";
+
     [Required]
     [EnumDataType(typeof(Role))]
-    public string Role { get; set; }
+    public string Role { get; set; } = "User";
     
     [Required]
     [MinLength(6)]
